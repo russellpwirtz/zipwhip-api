dbe19b62f2d44ac8ef35f131652dc36a3f1c097b
Jeremy Dinsel <jdinsel@zipwhip.com>
2012-01-26 15:09:39 -0800

    Handle normal Dates

--- src/main/java/com/zipwhip/util/JsonDateUtil.java
+++ src/main/java/com/zipwhip/util/JsonDateUtil.java
@@ -1,90 +1,102 @@
 package com.zipwhip.util;
 
-import org.apache.log4j.Logger;
-
 import java.text.Format;
 import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
+
+import org.apache.log4j.Logger;
 
 public class JsonDateUtil {
 
-    private static final Format GRAILS_DATE_FORMATTER = new ISO8601DateTimeFormat();
-    private static final Logger LOGGER = Logger.getLogger(JsonDateUtil.class);
+	private static final Format GRAILS_DATE_FORMATTER = new ISO8601DateTimeFormat();
+	private static final Logger LOGGER = Logger.getLogger(JsonDateUtil.class);
 
-    /**
-     * Utility class --> private constructor
-     */
-    private JsonDateUtil() {
-    }
+	private static final SimpleDateFormat formatter = new SimpleDateFormat("EEE MMM dd HH:mm:ss zzz yyyy");
 
-    /**
-     * Takes an object and returns a java.util.Date.
-     * Supported objects are String (format: yyyy-MM-ddTHH:mm:ssZ)
-     * and Long (date in milli seconds)
-     * The time zone for the input date string is considered to be in GMT by
-     * default (+0000)
-     * Returns null in case of an exception
-     *
-     * @param date - Standard Grails Formatted String Date
-     * @return Date - java.util.Date object
-     */
-    public static Date getDate(Object date) {
+	/**
+	 * Utility class --> private constructor
+	 */
+	private JsonDateUtil() {
+	}
 
-        //Check for null
-        if (date == null) {
-            return null;
-        }
+	/**
+	 * Takes an object and returns a java.util.Date.
+	 * Supported objects are String (format: yyyy-MM-ddTHH:mm:ssZ)
+	 * and Long (date in milli seconds)
+	 * The time zone for the input date string is considered to be in GMT by
+	 * default (+0000)
+	 * Returns null in case of an exception
+	 *
+	 * @param date - Standard Grails Formatted String Date
+	 * @return Date - java.util.Date object
+	 */
+	public static Date getDate(Object date) {
 
-        //Call the right parser method depending on the input datatype
-        if (date instanceof String) {
-            return getDate(date.toString());
-        } else if (date instanceof Long) {
-            long longDate = (Long) date;
-            return getDate(longDate);
-        } else if (date instanceof Date) { // we need to handle the backwards compatible case
-            return (Date) date;
-        }
+		//Check for null
+		if (date == null) {
+			return null;
+		}
 
-        LOGGER.warn("Unsupported datatype! " + date.getClass().getName());
-        return null;
-    }
+		//Call the right parser method depending on the input datatype
+		if (date instanceof String) {
+			return getDate(date.toString());
+		} else if (date instanceof Long) {
+			long longDate = (Long) date;
+			return getDate(longDate);
+		} else if (date instanceof Date) { // we need to handle the backwards compatible case
+			return (Date) date;
+		}
 
-    /**
-     * Takes a string date in the grails default format (yyyy-MM-ddTHH:mm:ssZ)
-     * and returns a java.util.Date
-     * The time zone for the input date string is considered to be in GMT by
-     * default (+0000)
-     * Returns null in case of an exception
-     *
-     * @param stringDate - Standard Grails Formatted String Date
-     * @return Date - java.util.Date
-     */
-    public static Date getDate(String stringDate) {
+		LOGGER.warn("Unsupported datatype! " + date.getClass().getName());
+		return null;
+	}
 
-        //Check for null, string length
-        if (stringDate == null || stringDate.length() <= 0) {
-            return null;
-        }
+	/**
+	 * Takes a string date in the grails default format (yyyy-MM-ddTHH:mm:ssZ)
+	 * and returns a java.util.Date
+	 * The time zone for the input date string is considered to be in GMT by
+	 * default (+0000)
+	 * Returns null in case of an exception
+	 *
+	 * @param stringDate - Standard Grails Formatted String Date
+	 * @return Date - java.util.Date
+	 */
+	public static Date getDate(String stringDate) {
 
-        //Parse and return date
-        try {
-            synchronized (GRAILS_DATE_FORMATTER) {
-                return (Date) GRAILS_DATE_FORMATTER.parseObject(stringDate.toUpperCase());
-            }
-        } catch (ParseException pex) {
-            LOGGER.error(pex);
-            return null;
-        }
-    }
+		//Check for null, string length
+		if ((stringDate == null) || (stringDate.length() <= 0)) {
+			return null;
+		}
 
-    /**
-     * Takes a long date timestamp and returns a java.util.Date
-     *
-     * @param milliDate - date in milli seconds
-     * @return Date - java.util.Date
-     */
-    public static Date getDate(long milliDate) {
-        return new Date(milliDate);
-    }
+		try {
+			Date parse = formatter.parse(stringDate);
+			if (parse != null) {
+				return parse;
+			}
+		} catch (ParseException e) {
+			// fall back to the Grails thing
+		}
+
+		// Parse and return date
+		try {
+			synchronized (GRAILS_DATE_FORMATTER) {
+				return (Date) GRAILS_DATE_FORMATTER.parseObject(stringDate.toUpperCase());
+			}
+		} catch (ParseException pex) {
+			LOGGER.error(pex);
+			return null;
+		}
+	}
+
+	/**
+	 * Takes a long date timestamp and returns a java.util.Date
+	 *
+	 * @param milliDate - date in milli seconds
+	 * @return Date - java.util.Date
+	 */
+	public static Date getDate(long milliDate) {
+		return new Date(milliDate);
+	}
 
 }
--- src/test/java/com/zipwhip/util/JsonDateUtilTest.java
+++ src/test/java/com/zipwhip/util/JsonDateUtilTest.java
@@ -1,13 +1,18 @@
 package com.zipwhip.util;
 
-import com.zipwhip.api.Address;
-import junit.framework.Assert;
-import org.junit.Before;
-import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
+
+import junit.framework.Assert;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.zipwhip.api.Address;
 
 /**
  * Created by IntelliJ IDEA.
@@ -17,52 +22,62 @@ import java.util.GregorianCalendar;
  */
 public class JsonDateUtilTest {
 
-    @Before
-    public void setUp() throws Exception {
+	@Before
+	public void setUp() throws Exception {
 
-    }
+	}
 
-    @Test
-    public void testGetDateFromObject() throws Exception {
+	@Test
+	public void testGetDateFromObject() throws Exception {
 
-        Date d = JsonDateUtil.getDate(null);
-        Assert.assertNull(d);
+		Date d = JsonDateUtil.getDate(null);
+		Assert.assertNull(d);
 
-        d = JsonDateUtil.getDate(new Address());
-        Assert.assertNull(d);
-    }
+		d = JsonDateUtil.getDate(new Address());
+		Assert.assertNull(d);
+	}
 
-    @Test
-    public void testGetDateFromString() throws Exception {
+	@Test
+	public void testGetDateFromString() throws Exception {
 
-        Date d = JsonDateUtil.getDate(StringUtil.EMPTY_STRING);
-        Assert.assertNull(d);
+		Date d = JsonDateUtil.getDate(StringUtil.EMPTY_STRING);
+		Assert.assertNull(d);
 
-        String dateString = "2011-09-12T11:01:02";
-        d = JsonDateUtil.getDate(dateString);
-        Assert.assertNotNull(d);
+		String dateString = "2011-09-12T11:01:02";
+		d = JsonDateUtil.getDate(dateString);
+		Assert.assertNotNull(d);
 
-        Calendar c = new GregorianCalendar();
-        c.setTime(d);
+		Calendar c = new GregorianCalendar();
+		c.setTime(d);
 
-        // Yes, MONTH is 0 based as opposed to all the other fields, lame
-        Assert.assertEquals(c.get(Calendar.MONTH), 8);
-        Assert.assertEquals(c.get(Calendar.YEAR), 2011);
-        Assert.assertEquals(c.get(Calendar.DAY_OF_MONTH), 12);
-        Assert.assertEquals(c.get(Calendar.HOUR_OF_DAY), 11);
-        Assert.assertEquals(c.get(Calendar.MINUTE), 1);
-        Assert.assertEquals(c.get(Calendar.SECOND), 2);
-    }
+		// Yes, MONTH is 0 based as opposed to all the other fields, lame
+		Assert.assertEquals(c.get(Calendar.MONTH), 8);
+		Assert.assertEquals(c.get(Calendar.YEAR), 2011);
+		Assert.assertEquals(c.get(Calendar.DAY_OF_MONTH), 12);
+		Assert.assertEquals(c.get(Calendar.HOUR_OF_DAY), 11);
+		Assert.assertEquals(c.get(Calendar.MINUTE), 1);
+		Assert.assertEquals(c.get(Calendar.SECOND), 2);
+	}
 
-    @Test
-    public void testGetDateFromTimestamp() throws Exception {
+	@Test
+	public void testGetDateFromTimestamp() throws Exception {
 
-        Date d1 = new Date();
+		Date d1 = new Date();
 
-        Date d2 = JsonDateUtil.getDate(d1.getTime());
+		Date d2 = JsonDateUtil.getDate(d1.getTime());
 
-        Assert.assertNotNull(d2);
-        Assert.assertEquals(d1, d2);
-    }
+		Assert.assertNotNull(d2);
+		Assert.assertEquals(d1, d2);
+	}
+
+	@Test
+	public void testGetANormalDate() {
+		Date date = new Date();
+
+		Date theDate = JsonDateUtil.getDate(date.toString());
+		assertNotNull(theDate);
+
+		assertEquals(date.toString(), theDate.toString());
+	}
 
 }
