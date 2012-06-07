afaf6f501ee300a6101e159b3bec841ab4d559b0
jedhoffmann <jedhoffmann@gmail.com>
2012-06-07 16:07:53 -0700

    Upgrading to netty 3.5.0.Beta1 and excluding netty from ning.

--- src/main/java/com/zipwhip/api/signals/sockets/netty/SignalConnectionBase.java
+++ src/main/java/com/zipwhip/api/signals/sockets/netty/SignalConnectionBase.java
@@ -37,14 +37,8 @@ public abstract class SignalConnectionBase extends CascadingDestroyableBase impl
 
     public static final int CONNECTION_TIMEOUT_SECONDS = 45;
 
-    private static final int DEFAULT_PING_TIMEOUT = 1000 * 300; // when to ping,
-    // inactive
-    // seconds
-    private static final int DEFAULT_PONG_TIMEOUT = 1000 * 30; // when to
-    // disconnect if
-    // a ping was
-    // not ponged by
-    // this time
+    private static final int DEFAULT_PING_TIMEOUT = 1000 * 300; // when to ping inactive seconds
+    private static final int DEFAULT_PONG_TIMEOUT = 1000 * 30; // when to disconnect if a ping was not ponged by this time
 
     private String host = "74.209.177.242";
     private int port = 3000;
