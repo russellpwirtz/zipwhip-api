a4157397e9d8f58f343b49aa56f21d656fc02224
jedhoffmann <jedhoffmann@gmail.com>
2012-08-20 11:27:21 -0700

    Removing a method in SignalConnectionDelegate which was not pushed in Michael's previous commit.

--- src/main/java/com/zipwhip/api/signals/sockets/netty/SignalConnectionDelegate.java
+++ src/main/java/com/zipwhip/api/signals/sockets/netty/SignalConnectionDelegate.java
@@ -14,14 +14,14 @@ import java.util.concurrent.Callable;
  * User: Michael
  * Date: 8/14/12
  * Time: 3:36 PM
- *
+ * <p/>
  * We can't allow the connections to talk with the SignalConnection directly. The problem is that they operate on a number
  * of crazy threads and theoretically 2 ChannelHandlers will access the same state of the parent object.
- *
+ * <p/>
  * To solve this problem we need a way to destroy the ChannelHandler.. But we can't since it's in a list. The other
  * suggestion is to compare the channel on-create to the channel on-event. However we can't pass it into the constructor
  * so that defeats the purpose.
- *
+ * <p/>
  * So we're going to use a Delegate that we can destroy. That way we can say: "Hey you're stale, kill yourself"
  */
 public class SignalConnectionDelegate extends DestroyableBase {
@@ -101,18 +101,6 @@ public class SignalConnectionDelegate extends DestroyableBase {
 
         connection.reconnectStrategy.stop();
     }
-
-    public synchronized void cancelOutstandingPingPongs() {
-        ensureValid();
-
-        connection.cancelOutstandingPingPongs();
-    }
-
-//    public void setWrapper(ChannelWrapper wrapper) {
-//        ensureValid();
-//
-//        connection.wrapper = wrapper;
-//    }
 
     public synchronized void notifyDisconnect(Object sender, boolean networkDisconnect) {
         ensureValid();
