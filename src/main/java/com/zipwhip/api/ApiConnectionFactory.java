855428dd6baadfd24722514ec7e417b722f77532
devserghini <dev.serghini@gmail.com>
2012-12-20 11:11:17 -0800

    zipwhip-api proxy upgrade to v 1.25

--- src/main/java/com/zipwhip/api/ApiConnectionFactory.java
+++ src/main/java/com/zipwhip/api/ApiConnectionFactory.java
@@ -5,13 +5,20 @@ import com.zipwhip.api.response.ResponseParser;
 import com.zipwhip.api.response.ServerResponse;
 import com.zipwhip.api.response.StringServerResponse;
 import com.zipwhip.concurrent.ObservableFuture;
+import com.zipwhip.net.ProxyUtil;
+import com.zipwhip.util.CollectionUtil;
 import com.zipwhip.util.Factory;
 import com.zipwhip.util.SignTool;
 import com.zipwhip.util.StringUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.net.MalformedURLException;
+import java.net.NoRouteToHostException;
+import java.net.Proxy;
+import java.net.URL;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -30,6 +37,68 @@ public abstract class ApiConnectionFactory implements Factory<ApiConnection> {
     private String apiKey;
     private String secret;
     private String sessionKey;
+    private Proxy proxy;
+
+    public ApiConnectionFactory() throws NoRouteToHostException {
+        this(Proxy.NO_PROXY);
+    }
+
+    /**
+     * ApiConnectionFactory constructor
+     *
+     * @param proxy - connection proxy
+     * @throws NoRouteToHostException   if there is no network access to a test host. (user behind a firewall or using a proxy)
+     * @throws IllegalArgumentException if proxy is null
+     */
+    public ApiConnectionFactory(final Proxy proxy) throws NoRouteToHostException {
+        super();
+        if (proxy == null) throw new IllegalArgumentException("Proxy cannot be null");
+        if (!Proxy.Type.DIRECT.equals(proxy.type())) this.proxy = proxy;
+        init();
+    }
+
+    private void init() throws NoRouteToHostException {
+        //1. Check if there is direct internet access
+        if (testDirectAccess()) return;
+
+        //2. try the local proxy if available
+        final URL url;
+        try {
+            url = new URL(ProxyUtil.DEFAULT_HTTP_TEST_URL);
+        } catch (MalformedURLException e) {
+            throw new IllegalStateException("Invalid URL: " + ProxyUtil.DEFAULT_HTTP_TEST_URL, e);
+        }
+        if (testProxyAccess(url)) return;
+
+        //3. try to auto detect the proxy
+        if (detectProxy(url)) return;
+
+        // We should not reach this statement if we are able to get through
+        throw new NoRouteToHostException("Failed to connect to host: " + ProxyUtil.DEFAULT_HTTP_TEST_URL);
+    }
+
+    private boolean testDirectAccess() {
+        return ProxyUtil.testInternetAccess();
+    }
+
+    private boolean testProxyAccess(final URL url) {
+        return proxy != null && ProxyUtil.testInternetAccess(proxy, url);
+    }
+
+    private boolean detectProxy(final URL url) throws NoRouteToHostException {
+        final List<Proxy> proxies = ProxyUtil.getProxy();
+        if (CollectionUtil.isNullOrEmpty(proxies)) return false;
+
+        for (Proxy proxy : proxies) {
+            // test for http
+            if (ProxyUtil.testInternetAccess(proxy, url)) {
+                this.proxy = proxy;
+                return true;
+            }
+        }
+
+        return false;
+    }
 
     /**
      * Creates a generic unauthenticated ApiConnection.
@@ -151,4 +220,12 @@ public abstract class ApiConnectionFactory implements Factory<ApiConnection> {
     public void setSessionKey(String sessionKey) {
         this.sessionKey = sessionKey;
     }
+
+    public Proxy getProxy() {
+        return proxy;
+    }
+
+//    public void setProxy(Proxy proxy) {
+//        this.proxy = proxy;
+//    }
 }
