5afcc25b159f004737b77069cd78497ebdc676d3
Michael Smyers <msmyers@zipwhip.com>
2013-09-10 14:46:26 -0700

    handoff to Russ. Breaking build.

---
+++ src/main/java/com/zipwhip/api/signals/dto/SignalContentUtil.java
@@ -0,0 +1,24 @@
+package com.zipwhip.api.signals.dto;
+
+import com.zipwhip.util.Converter;
+import com.zipwhip.util.DataConversionException;
+
+import java.util.HashMap;
+
+/**
+ * Date: 9/10/13
+ * Time: 2:41 PM
+ *
+ * @author Michael
+ * @version 1
+ */
+public class SignalContentUtil {
+
+    public HashMap<String, Object> convert(SignalContact content) throws DataConversionException {
+        return null;
+    }
+
+    public HashMap<String, Object> convert(SignalMessage content) throws DataConversionException {
+        return null;
+    }
+}
